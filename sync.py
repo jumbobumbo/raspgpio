6c86b1f86a2ac5c02e5d0b1a88a5ee65a60e7013
dbwheeler <dwheels91@gmail.com>
2019-09-28 14:03:08 +0100

    move pi connection into the for loop, wrapped in a try. Not as pretty, but allows the sync to continue if pi conn fails.

--- sync.py
+++ sync.py
@@ -11,16 +11,8 @@ from pathlib import Path
 # Open conf file (read json for IP and connect)
 conf_file = json_reader.JSONReads.reader(config_selector.ConfSelect.selection())  # use default file and open it
 
-"""  Create config reader and connection objects"""
+"""  Create config reader """
 conf_reader = json_reader.JSONReads
-pi_conn = connector.PiConnect.single_factory_connector(conf_reader.finder(conf_file, "connection", "ip_add"))
-
-""" Get an LED board up and running """
-# read LED values from conf
-LED_pins = conf_reader.finder(conf_file, "LEDs")
-# Set the board up
-LED_board = led_conn.LED(data_handlers.HandleData.dict_to_value_list(LED_pins), pi_conn)
-LED_board = LED_board.board()
 
 """ FILE SYNC CONFIG """
 # regex for ignore files
@@ -28,8 +20,23 @@ regex = r".+\.(txt|log|git|jar)$"
 
 """ COPY THE FILES """
 for s, d in conf_reader.finder(conf_file, "dirs").items():
-        LED_board.blink()
-        sync(Path(s), Path(d), "sync", verbose=False, exclude=(regex,))
+    """ try and connect - want to allow back to run even if pi conn fails """
+    try:
+        pi_conn = connector.PiConnect.single_factory_connector(conf_reader.finder(conf_file, "connection", "ip_add"))
 
-# complete
-TearDown.down(LED_board)
+        """ Get an LED board up and running """
+        # read LED values from conf
+        LED_pins = conf_reader.finder(conf_file, "LEDs")
+        # Set the board up
+        LED_board = led_conn.LED(data_handlers.HandleData.dict_to_value_list(LED_pins), pi_conn)
+        LED_board = LED_board.board()
+        LED_board.blink()
+    except (IOError, OSError) as error:
+        print(f"{error} occurred when connecting to PI")
+    # run the sync process
+    sync(Path(s), Path(d), "sync", verbose=False, exclude=(regex,))
+    """ wrap cleanup in try just in case pi conn fails"""
+    try:
+        TearDown.down(LED_board)
+    except Exception as ex:
+        print(ex)
