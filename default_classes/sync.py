5322aaf1e95e58f577578b56c093a14bd3d61089
jumbobumbo <MasterCh0der!*>
2019-12-18 22:19:01 +0000

    didn't need that loop anyway

--- default_classes/sync.py
+++ default_classes/sync.py
@@ -53,8 +53,8 @@ class LEDSyncer(Syncer):
             # open up the LED board connection (LED pin values, single pi conn obj)
             with conn.LED(dh.HandleData.dict_to_value_list(self.conf_data[self.LED_key]),
                           connector.PiConnect.single_factory_connector(self.conf_data[self.ip_key])).board() as pi:
-                for i in [pi.blink, self.file_sync, pi.blink]:
-                    i()  # blink, sync, blink
+                pi.blink()  # LEDs blink
+                self.file_sync()  # files sync
         except (IOError, OSError) as error:  # connection rejections
             print(f"{error} occurred when connecting to PI. Will sync without blink")
             self.file_sync()  # still sync the data
