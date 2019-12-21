72ec7a35715868e335cbf9362a9f483b2fccbf84
jumbobumbo <MasterCh0der!*>
2019-12-21 10:28:42 +0000

    fixed to actually use config selector class

--- sync_plex.py
+++ sync_plex.py
@@ -1,9 +1,8 @@
-from pathlib import Path
 from default_classes.sync import LEDSyncer
-
+from startup_classes.config_selector import ConfSelect as conf
 
 LEDSyncer("pi_ip_add", "LEDs",
           r".+\.(txt|log|git|jar)$",
           "plex",
-          Path(str(Path.cwd()), "config/", "default_config.json")
+          conf.selection()
           ).file_sync_LED()
